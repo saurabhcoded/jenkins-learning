ffc82f2c2d929fae331bd018670841e66dbea71f
saurabhcoded <saurabhcoded@gmail.com>
2023-10-21 22:20:01 +0530

    Added Readme File

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send('<h2>Api are working fine...</h2>');
+    res.send('<h2>Webhook Implemented</h2>');
 })
 app.get("/test", (req, res) => {
     res.send('<h2>Test Page</h2>');
